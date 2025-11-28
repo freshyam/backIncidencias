e6201a399e485c289bcb01f133f7caff77fc87b3
yamir <ingpaezayala@gmail.com>
2025-11-28 12:52:37 -0700

    fix bd_port

--- Config/connectionSQL.js
+++ Config/connectionSQL.js
@@ -1,14 +1,17 @@
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
-const connection =  mysql.createPool({
-    host    :   process.env.HOST,
-    port    :   process.env.DB_PORT,
-    user    :   process.env.USER_BD, 
-    password:   process.env.PASSWORD, 
-    database:   process.env.DATABASE,
-    
+const connection = mysql.createPool({
+  host: process.env.HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.USER_BD,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
+
 
 export default connection
 
